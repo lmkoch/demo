5b5adc5d41939aaa8c5424b392da6ceb651e1efa
Lisa Koch <lmkoch@users.noreply.github.com>
2022-04-28 09:59:00 +0200

    initial commit.

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+
+
+def add_two_numbers(a, b):
+    return a + b + 1
+
+if __name__ == '__main__':
+    
+    a = 2
+    b = 3
+    
+    print(f'{a} + {b} = {add_two_numbers(a, b)}')
+    
+    
+    
